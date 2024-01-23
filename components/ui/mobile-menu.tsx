e80646b68617fee0ff66541c49221cedcddbcfb4
David Karim <david@quiknode.io>
2024-01-22 22:35:36 -0500

    Another update for mobile menu

--- components/ui/mobile-menu.tsx
+++ components/ui/mobile-menu.tsx
@@ -63,11 +63,13 @@ export default function MobileMenu() {
       >
         <ul className="bg-gray-800 px-4 py-2">
           <li>
-            <Link
-              href={`${app_endpoint}`}
-              className="font-medium w-full inline-flex items-center justify-center border border-transparent px-4 py-2 my-2 rounded-sm text-white bg-purple-600 hover:bg-purple-700 transition duration-150 ease-in-out" onClick={() => setMobileNavOpen(false)}>
+            <a
+              href={app_endpoint}
+              className="font-medium w-full inline-flex items-center justify-center border border-transparent px-4 py-2 my-2 rounded-sm text-white bg-purple-600 hover:bg-purple-700 transition duration-150 ease-in-out"
+              onClick={() => setMobileNavOpen(false)}
+            >
               Enter App
-            </Link>
+            </a>
           </li>
           <li>
             <Link
