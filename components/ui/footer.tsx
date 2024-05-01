a6a7f19a28ad126abc72579ed186e9ab2261f554
David Karim <david@quiknode.io>
2024-05-01 12:08:50 -0400

    X link and other footer updates

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -29,13 +29,13 @@ export default function Footer() {
                 <h6 className="text-gray-200 font-medium mb-1">Highlights</h6>
                 <ul>
                   <li className="mb-1">
-                    <Link href="/" className="text-gray-400 hover:text-gray-100 transition duration-150 ease-in-out">CrowdFund</Link>
+                    <Link href="https://app.bocacontracts.com/contractTemplate/23?type=2" className="text-gray-400 hover:text-gray-100 transition duration-150 ease-in-out">Smart Wallet</Link>
                   </li>
                   <li className="mb-1">
-                    <Link href="/" className="text-gray-400 hover:text-gray-100 transition duration-150 ease-in-out">Lottery</Link>
+                    <Link href="https://app.bocacontracts.com/contractTemplate/22?type=1" className="text-gray-400 hover:text-gray-100 transition duration-150 ease-in-out">Lottery</Link>
                   </li>
                   <li className="mb-1">
-                    <Link href="/" className="text-gray-400 hover:text-gray-100 transition duration-150 ease-in-out">Hire a Contractor</Link>
+                    <Link href="https://app.bocacontracts.com/contractTemplate/21?type=0" className="text-gray-400 hover:text-gray-100 transition duration-150 ease-in-out">CrowdFund</Link>
                   </li>
                 </ul>
               </div>
@@ -82,7 +82,7 @@ export default function Footer() {
             {/* Social links */}
             <ul className="flex mb-4 md:order-1 md:ml-4 md:mb-0">
               <li>
-                <Link href="/" className="flex justify-center items-center text-purple-600 bg-gray-800 hover:text-gray-100 hover:bg-purple-600 rounded-full transition duration-150 ease-in-out" aria-label="Twitter">
+                <Link href="https://twitter.com/bocacontracts" className="flex justify-center items-center text-purple-600 bg-gray-800 hover:text-gray-100 hover:bg-purple-600 rounded-full transition duration-150 ease-in-out" aria-label="Twitter">
                   <svg className="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
                     <path d="M24 11.5c-.6.3-1.2.4-1.9.5.7-.4 1.2-1 1.4-1.8-.6.4-1.3.6-2.1.8-.6-.6-1.5-1-2.4-1-1.7 0-3.2 1.5-3.2 3.3 0 .3 0 .5.1.7-2.7-.1-5.2-1.4-6.8-3.4-.3.5-.4 1-.4 1.7 0 1.1.6 2.1 1.5 2.7-.5 0-1-.2-1.5-.4 0 1.6 1.1 2.9 2.6 3.2-.3.1-.6.1-.9.1-.2 0-.4 0-.6-.1.4 1.3 1.6 2.3 3.1 2.3-1.1.9-2.5 1.4-4.1 1.4H8c1.5.9 3.2 1.5 5 1.5 6 0 9.3-5 9.3-9.3v-.4c.7-.5 1.3-1.1 1.7-1.8z" />
                   </svg>
